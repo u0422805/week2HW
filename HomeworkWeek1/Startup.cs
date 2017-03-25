c7c5e33347460d291fbb591f689fd73902b471bd
USER <USER@USER-PC>
2017-03-25 22:33:19 +0800

    加入專案檔案。

---
+++ HomeworkWeek1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeworkWeek1.Startup))]
+namespace HomeworkWeek1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
